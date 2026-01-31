0c8bc85cd9f0260ad01888b5b348a771ff4a85ad
yeiconcr <yeiconcr@gmail.com>
2026-01-30 22:36:46 -0500

    feat: configurar panel de administración Filament en español
    - Publicar traducciones de Filament (vendor:publish)
    - Configurar locale 'es' en AdminPanelProvider
    - Panel ahora muestra todo en español (botones, mensajes, validaciones)
    - Mantiene configuración de timezone America/Bogota

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->brandName('MH Consultores')
             ->favicon(asset('favicon.ico'))
+            ->locale('es')  // Configurar panel en español
+            ->defaultLocale('es')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
